42084ba72d9ed980ea4c395d298b065d4c40b2ca
Briks Chiki <sgs7edged@gmail.com>
2022-06-02 13:44:06 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DudnikAlex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
